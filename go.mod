84c11d0d5fa76d99d71d7fba61f123c2d59d7bd3
shankusu <shankusu2017@gmail.com>
2024-05-15 10:18:58 +0800

    little

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/shankusu2017/utils
 go 1.21
 
 require github.com/google/gopacket v1.1.19
+
+require github.com/shankusu2017/constant v0.0.0-20240514114055-1c970a2b30c1 // indirect
--- pac_test.go
+++ pac_test.go
@@ -2,19 +2,29 @@ package utils
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"testing"
 )
 
 func TestIPConv(t *testing.T) {
 	fmt.Printf(os.Getwd())
-	InitPac("../config/cnIP.cfg")
+	InitPac("../OpenVPN-Golang/config/cnIP.cfg")
 	if IsLocalIP("47.103.204.157") != true {
 		t.Fatalf("8a48b8f6 FATAL")
 	}
 	if IsLocalIP("8.8.8.8") {
 		t.Fatalf("666d1174 FATAL")
 	}
-	log.Printf("isLocalIP:%v\n", IsLocalIP("182.92.205.225"))
+	if IsLocalIP("116.63.131.209") != true {
+		t.Fatalf("0x08c817a8 FATAL")
+	}
+	if IsLocalIP("64.176.173.228") == true {
+		t.Fatalf("0x1ecbf52d FATAL")
+	}
+	if IsLocalIP("64.176.85.253") == true {
+		t.Fatalf("0x3eb80066 FATAL")
+	}
+	if IsLocalIP("182.92.205.225") != true {
+		t.Fatalf("0x5c68b8eb FATAL")
+	}
 }
